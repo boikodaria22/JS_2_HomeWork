9c28dff08a31e653ba8f18fa58200691d2a28589
Daria <79093855+boikodaria22@users.noreply.github.com>
2021-10-13 23:38:56 +0300

    Update HomeWorkJS_2.3.js

--- JS_2_HomeWork/JS_HW-2/js/HomeWorkJS_2.3.js
+++ JS_2_HomeWork/JS_HW-2/js/HomeWorkJS_2.3.js
@@ -1,18 +1,18 @@
 function showStudentsFavoriteLessons() {
-	var lessons = ['Математика', 'История', 'Спорт', 'Литература', 'Биология', 'География'];
-	for (var i = 0; i < arguments.length; i++) {
-		var listOfFavoriteLessons = checkStudentFavoriteLessons(arguments[i], lessons);
-		console.log(listOfFavoriteLessons);
-	}
+  var lessons = ['Математика', 'История', 'Спорт', 'Литература', 'Биология', 'География'];
 
-	function checkStudentFavoriteLessons(nameStudent, arr) {
-		var n = Math.floor(Math.random() * (arr.length - 1));
-		var favoriteLesson = arr.slice(arr.length - n);
-		if (favoriteLesson.length > 0) {
-			return `${nameStudent}: ${favoriteLesson.join(', ')}`;
-		} else {
-			return `${nameStudent}: У этого студента нет любимых предметов`;
-		}
-	}
+  function checkStudentFavoriteLessons(nameStudent, arr) {
+    var n = Math.floor(Math.random() * (arr.length - 1));
+    var favoriteLesson = arr.slice(arr.length - n);
+    if (favoriteLesson.length > 0) {
+      return `${nameStudent}: ${favoriteLesson.join(', ')}`;
+    } else {
+      return `${nameStudent}: У этого студента нет любимых предметов`;
+    }
+  }
+  for (var i = 0; i < arguments.length; i++) {
+    var listOfFavoriteLessons = checkStudentFavoriteLessons(arguments[i], lessons);
+    console.log(listOfFavoriteLessons);
+  }
 }
 showStudentsFavoriteLessons('Артур', 'Женя', 'Костя', 'Маша', 'Денис');
