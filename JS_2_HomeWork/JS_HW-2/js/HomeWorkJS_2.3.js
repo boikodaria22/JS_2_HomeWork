804b9a39b50bdcff4755fb49d0515e9e69edf8c6
Daria <79093855+boikodaria22@users.noreply.github.com>
2021-10-15 18:55:59 +0300

    Update HomeWorkJS_2.3.js

--- JS_2_HomeWork/JS_HW-2/js/HomeWorkJS_2.3.js
+++ JS_2_HomeWork/JS_HW-2/js/HomeWorkJS_2.3.js
@@ -1,18 +1,18 @@
 function showStudentsFavoriteLessons() {
   var lessons = ['Математика', 'История', 'Спорт', 'Литература', 'Биология', 'География'];
 
-  function checkStudentFavoriteLessons(nameStudent, arr) {
-    var n = Math.floor(Math.random() * (arr.length - 1));
-    var favoriteLesson = arr.slice(arr.length - n);
+  function checkStudentFavoriteLessons(studentName) {
+    var n = Math.floor(Math.random() * (lessons.length - 1));
+    var favoriteLesson = lessons.slice(lessons.length - n);
     if (favoriteLesson.length > 0) {
-      return `${nameStudent}: ${favoriteLesson.join(', ')}`;
+      return `${studentName}: ${favoriteLesson.join(', ')}`;
     } else {
-      return `${nameStudent}: У этого студента нет любимых предметов`;
+      return `${studentName}: У этого студента нет любимых предметов`;
     }
   }
   for (var i = 0; i < arguments.length; i++) {
-    var listOfFavoriteLessons = checkStudentFavoriteLessons(arguments[i], lessons);
-    console.log(listOfFavoriteLessons);
+   
+    console.log(checkStudentFavoriteLessons(arguments[i]));
   }
 }
 showStudentsFavoriteLessons('Артур', 'Женя', 'Костя', 'Маша', 'Денис');
